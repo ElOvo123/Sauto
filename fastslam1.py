ca9d40498af25a323f0a7cfd3a2a697d7a209970
DiogoPi <pica.diogomiguel@gmail.com>
2026-05-19 12:37:09 +0100

    added a scallar to be to useful

--- fastslam1.py
+++ fastslam1.py
@@ -96,12 +96,14 @@ class FastSLAM1(ParticleFilter):
         # Sideways movement should be small for differential-drive robot.
         # We ignore local_dy to avoid fake rotations caused by odometry noise.
         rot1 = 0.0
-        rot2 = (current_odom[2] - self.prev_odom[2] + math.pi) % (2 * math.pi) - math.pi
+        yaw_gain = 0.90
+        rot2 = math.atan2(math.sin(current_odom[2] - self.prev_odom[2]), math.cos(current_odom[2] - self.prev_odom[2]))
+        rot2 *= yaw_gain        
         rot_total = rot2
 
         # --- A BARREIRA ESPACIAL ---
         # Se não andou o suficiente nem rodou o suficiente, devolve as poses antigas e NÃO faz nada!
-        if trans < self.min_trans_update and abs(rot_total) < self.min_rot_update:
+        if abs(trans) < self.min_trans_update and abs(rot_total) < self.min_rot_update:
             
             # Exceção: Se for o primeiríssimo frame, queremos mapear o que está à volta antes de arrancar
             if not self.is_initialized and len(measurements) > 0:
